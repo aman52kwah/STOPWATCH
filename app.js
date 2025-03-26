4ac8bda9b21ad5aea37f4287504490b7e33f9a82
aman52kwah <amankwaamichael52@gmail.com>
2025-03-26 08:30:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// create variable for btn
+
+const startStopBtn =document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+// variables for time values
+let seconds = 0;
+let minutes= 0;
+let hours = 0;
+
